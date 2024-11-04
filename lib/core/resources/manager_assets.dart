dbd9d065f55956318e55dc7b1358efea21957a96
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-11-04 13:43:31 +0200

    add manager resources

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -1,5 +1,11 @@
 const String imagesPath = 'assets/images';
 
 class ManagerAssets {
-  static const String splashImg = '$imagesPath/splash_img.svg';
+  static const String splashImg = '$imagesPath/splash.png';
+  static const String homeStartImg = '$imagesPath/start.png';
+  static const String defaultUserImg = '$imagesPath/default_user.jpg';
+  static const String homeImg = '$imagesPath/home.svg';
+  static const String completedImg = '$imagesPath/finished.svg';
+  static const String searchImg = '$imagesPath/search.svg';
+  static const String settingsImg = '$imagesPath/settings.svg';
 }
--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,4 +1,11 @@
 class ManagerStrings {
   static const String noRouteFound = 'No Route Found';
   static const String appName = 'TODO APP';
+  static const String startTitle = 'Start Your Journey';
+  static const String startSubtitle =
+      'Every big step start with small step.\nNotes your first idea and start \nyour journey!';
+  static const String home = 'Home';
+  static const String search = 'Search';
+  static const String completed = 'Completed';
+  static const String settings = 'Settings';
 }
