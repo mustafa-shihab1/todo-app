66569ccbbf5b1933c08f4857edc23a37121566ba
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-11-04 13:06:58 +0200

    add device dimensions

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+import 'config/constants.dart';
 import 'routes/routes.dart';
 
 void main() {
@@ -10,11 +11,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         splitScreenMode: true,
         minTextAdapt: true,
+        designSize: const Size(
+          Constants.deviceWidth,
+          Constants.deviceHeight,
+        ),
         builder: (context, child) {
           return GetMaterialApp(
             debugShowCheckedModeBanner: false,
