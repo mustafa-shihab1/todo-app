fb843baab457af34eb2135bc711212562c115e46
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-11-04 13:35:20 +0200

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
+  static const double deviceWidth = 412;
+  static const double deviceHeight = 869;
   static const int splashDuration = 3;
+  static const double appBarElevation = 0;
 }
