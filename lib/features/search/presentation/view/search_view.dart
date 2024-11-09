77b2131ffb6611ed60a5218cf0b8e3499e948dfc
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-11-09 20:24:22 +0200

    create custom search box

--- lib/features/search/presentation/view/search_view.dart
+++ lib/features/search/presentation/view/search_view.dart
@@ -1,43 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/core/resources/manager_fonts.dart';
-import 'package:todo_app/core/resources/manager_styles.dart';
-
-import '../../../../core/resources/manager_colors.dart';
+import 'package:todo_app/core/resources/manager_colors.dart';
 import '../../../../core/resources/manager_sizes.dart';
-import '../../../../core/resources/manager_strings.dart';
+import '../widgets/custom_search_box.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: ManagerSymmetricEdgeInsets.v2h14,
-      child: Column(
-        children: [
-          SizedBox(
-            height: ManagerHeight.h40,
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: ManagerColors.softGreyColor,
-                borderRadius: BorderRadius.circular(10)),
-            child: TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: ManagerStrings.search,
-                  hintStyle: getRegularTextStyle(
-                      fontSize: ManagerFontSize.s12,
-                      color: ManagerColors.grey)),
-              style: getRegularTextStyle(
-                  fontSize: ManagerFontSize.s12, color: ManagerColors.black),
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        const Divider(
+          color: ManagerColors.dividerColor,
+          thickness: 2,
+        ),
+        SizedBox(
+          height: ManagerHeight.h20,
+        ),
+        const CustomSearchBox(),
+      ],
     );
   }
 }
