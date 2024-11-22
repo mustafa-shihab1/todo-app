bb90c39456fe92e37ed20baf03acbf36575eaaab
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-11-22 18:59:49 +0200

    edit home controller

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class HomeController extends GetxController {
   DateTime currentDate = DateTime.now();
+  DateTime selectedDate = DateTime.now();
   List<DateTime> currentMonthDays = [];
   int selectedDayIndex = DateTime.now().day - 1;
 
@@ -12,41 +12,16 @@ class HomeController extends GetxController {
 
   String getFullDate() => DateFormat('MMMM dd, y').format(currentDate);
 
-  String getDate(currentDate) => DateFormat('dd').format(currentDate);
+  String getDate(date) => DateFormat('dd').format(date);
 
-  String getDay(currentDate) => DateFormat('EEE').format(currentDate);
+  String getDay(date) => DateFormat('EEE').format(date);
 
-  String getMonth(currentDate) => DateFormat('MMM').format(currentDate);
+  String getMonth(date) => DateFormat('MMM').format(date);
 
-  int numberOfDaysInMonth(int year, int month) {
-    if (month == DateTime.february) {
-      return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
-    }
-    return [4, 6, 9, 11].contains(month) ? 30 : 31;
-  }
+  String getYear(date) => DateFormat('y').format(date);
 
-  void getCurrentMonthDays() {
-    final now = DateTime.now();
-    final days = numberOfDaysInMonth(now.year, now.month);
-
-    currentMonthDays = List.generate(
-      days,
-      (index) => DateTime(now.year, now.month, index + 1),
-    );
-  }
-
-  void changeSelectedDay(int index) {
-    selectedDayIndex = index;
+  void changeDay(DateTime date) {
+    selectedDate = date;
     update();
-  }
-
-  void scrollToCurrentDay(int index) => itemController.jumpTo(index: index);
-
-  @override
-  void onInit() {
-    getCurrentMonthDays();
-    super.onInit();
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) => scrollToCurrentDay(selectedDayIndex));
   }
 }
