188d24af059905db35eb29edea65bd434a5f274b
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-11-23 22:02:47 +0200

    inject settings controlles

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import '../features/home/presentation/controller/home_controller.dart';
 import '../features/main/presentation/controller/main_controller.dart';
+import '../features/settings/presentation/controller/settings_controller.dart';
 import '../features/splash/presentation/controller/splash_controller.dart';
 
 final instance = GetIt.instance;
@@ -17,5 +18,6 @@ disposeSplash() {
 initMain() {
   disposeSplash();
   Get.put<HomeController>(HomeController());
+  Get.put<SettingsController>(SettingsController());
   Get.put<MainController>(MainController());
 }
