e0e0e7d5f7150e5d42a35fa04fe5777cf5f72d6b
mstfa_201 <mostafaa.sh2001@gmail.com>
2025-03-23 17:12:58 +0200

    Create add_task_view.dart

---
+++ lib/features/tasks/presentation/view/add_task_view.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app/core/validator/validator.dart';
+import '../../../../core/resources/manager_colors.dart';
+import '../../../../core/resources/manager_fonts.dart';
+import '../../../../core/resources/manager_sizes.dart';
+import '../../../../core/resources/manager_strings.dart';
+import '../../../../core/resources/manager_styles.dart';
+import '../../../../core/widgets/custom_text_field.dart';
+import '../../../../core/widgets/main_button.dart';
+import '../../../main/presentation/view/custom_appbar.dart';
+import '../controller/task_controller.dart';
+
+class AddTaskView extends StatelessWidget {
+  const AddTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    FieldValidator validator = FieldValidator();
+    return Scaffold(
+      backgroundColor: ManagerColors.backgroundColor,
+      appBar: customHomeAppBar(isHomePage: false),
+      body: Padding(
+        padding:
+            ManagerSetEdgeInsets.setSymmetric(horizontal: ManagerWidth.w16),
+        child: SingleChildScrollView(
+          child: GetBuilder<TaskController>(
+              initState: (_) {
+                Get.find<TaskController>().clearControllers();
+              },
+              builder: (controller) {
+
+            return Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    ManagerStrings.addTask,
+                    style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s18,
+                        color: ManagerColors.black),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  Text(
+                    ManagerStrings.title,
+                    style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.black),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h6,
+                  ),
+                  customTextField(
+                      validator: (value) => validator.validateText(
+                            value,
+                          ),
+                      hintText: ManagerStrings.addTitle,
+                      controller: controller.titleController,
+                      radius: ManagerRadius.r10),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  Text(
+                    ManagerStrings.note,
+                    style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.black),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h6,
+                  ),
+                  customTextField(
+                      validator: (value) => validator.validateText(
+                            value,
+                          ),
+                      hintText: ManagerStrings.addNote,
+                      controller: controller.descController,
+                      radius: ManagerRadius.r10),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  Text(
+                    ManagerStrings.date,
+                    style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.black),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h6,
+                  ),
+                  customTextField(
+                      hintText: ManagerStrings.selectDate,
+                      controller: controller.dateController,
+                      radius: ManagerRadius.r10,
+                      suffixIcon: const Icon(
+                        Icons.calendar_month_outlined,
+                        color: ManagerColors.grey,
+                      ),
+                      readOnly: true,
+                      onTap: () => controller.selectDate(context)),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  Text(
+                    ManagerStrings.time,
+                    style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.black),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h6,
+                  ),
+                  customTextField(
+                      hintText: ManagerStrings.selectTime,
+                      controller: controller.timeController,
+                      radius: ManagerRadius.r10,
+                      suffixIcon: const Icon(
+                        Icons.watch_later_outlined,
+                        color: ManagerColors.grey,
+                      ),
+                      readOnly: true,
+                      onTap: () => controller.selectTime(context)),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  Text(
+                    ManagerStrings.repeat,
+                    style: getBoldTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.black),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h6,
+                  ),
+                  DropdownButtonFormField(
+                    hint: Text(
+                      'Select an option',
+                      style: getRegularTextStyle(
+                        fontSize: ManagerFontSize.s14,
+                        color: ManagerColors.greyLight,
+                      ),
+                    ),
+                    value: controller.selectedOption,
+                    items: controller.repeatOptions.map(
+                      (String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (value) => controller.selectedOption = value,
+                    borderRadius: BorderRadius.circular(ManagerRadius.r10),
+                    icon: const Icon(Icons.arrow_drop_down,
+                        color: ManagerColors.grey),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                          horizontal: ManagerWidth.w10,
+                          vertical: ManagerHeight.h10),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(ManagerRadius.r10),
+                          borderSide:
+                              const BorderSide(color: ManagerColors.greyLight)),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          ManagerRadius.r10,
+                        ),
+                        borderSide: const BorderSide(
+                          color: ManagerColors.greyLight,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          ManagerRadius.r10,
+                        ),
+                        borderSide: const BorderSide(
+                          color: ManagerColors.greyLight,
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          ManagerRadius.r10,
+                        ),
+                        borderSide: const BorderSide(
+                          color: ManagerColors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h60,
+                  ),
+                  MainButton(
+                      onPressed: () {
+                        controller.performCreation();
+                      },
+                      btnTitle: ManagerStrings.createTask,
+                      btnColor: ManagerColors.primaryColor,
+                      borderColor: ManagerColors.primaryColor,
+                      borderRadius: 12,
+                      titleColor: ManagerColors.white),
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
