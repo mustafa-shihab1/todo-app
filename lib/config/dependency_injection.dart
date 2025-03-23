c99c491ec556f9bd14defea01cb00e870eebc392
mstfa_201 <mostafaa.sh2001@gmail.com>
2025-03-23 17:17:10 +0200

    create initModule in di

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,11 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
+import '../core/storage/local/database/provider/database_provider.dart';
 import '../features/home/presentation/controller/home_controller.dart';
 import '../features/main/presentation/controller/main_controller.dart';
 import '../features/settings/presentation/controller/settings_controller.dart';
 import '../features/splash/presentation/controller/splash_controller.dart';
+import '../features/tasks/presentation/controller/task_controller.dart';
 
 final instance = GetIt.instance;
+
+initModule() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseProvider().initDatabase();
+}
 
 initSplash() {
   Get.put<SplashController>(SplashController());
@@ -19,5 +27,6 @@ initMain() {
   disposeSplash();
   Get.put<HomeController>(HomeController());
   Get.put<SettingsController>(SettingsController());
+  Get.put<TaskController>(TaskController());
   Get.put<MainController>(MainController());
 }
