8148450841d6ea08adf167c15c81bfa410d5951f
mstfa_201 <mostafaa.sh2001@gmail.com>
2025-03-28 07:04:07 +0200

    add new condition to crud op

--- lib/core/storage/local/database/controller/note_database_controller.dart
+++ lib/core/storage/local/database/controller/note_database_controller.dart
@@ -9,7 +9,9 @@ class NoteDatabaseController extends DatabaseOperations<Note> {
   Database? database = DatabaseProvider().database;
   @override
   Future<int> create(Note object) async {
-    return await database!.insert(Constants.notesTableName, object.toMap());
+    Map<String, dynamic> data = object.toMap();
+    data[Constants.notesIsCompletedColumnName] ??= 0;
+    return await database!.insert(Constants.notesTableName, data);
   }
 
   @override
@@ -21,9 +23,10 @@ class NoteDatabaseController extends DatabaseOperations<Note> {
 
   @override
   Future<List<Note>> read() async {
-    List<Map<String, Object?>> data =
-        await database!.query(Constants.notesTableName);
-
+    List<Map<String, Object?>> data = await database!.query(
+        Constants.notesTableName,
+        where: '${Constants.notesIsCompletedColumnName} = ?',
+        whereArgs: [0]);
     // List<Note> notes = [];
     // for (Map<String, Object?> note in data) {
     //   notes.add(Note.fromMap(note));
@@ -31,15 +34,6 @@ class NoteDatabaseController extends DatabaseOperations<Note> {
     // return notes;
     return data.map((row) => Note.fromMap(row)).toList();
   }
-
-  // @override
-  // Future<Note?> show(int id) async {
-  //   List<Map<String, Object?>> data = await database!.query(
-  //       Constants.notesTableName,
-  //       where: '${Constants.notesIdColumnName} = ?',
-  //       whereArgs: [id]);
-  //   return data.isNotEmpty ? Note.fromMap(data.first) : null;
-  // }
 
   @override
   Future<bool> update(Note object) async {
@@ -52,8 +46,24 @@ class NoteDatabaseController extends DatabaseOperations<Note> {
   Future<List<Note>?> searchByTitle(String title) async {
     List<Map<String, Object?>> data = await database!.query(
         Constants.notesTableName,
-        where: '${Constants.notesTitleColumnName} LIKE ?',
+        where:
+            '${Constants.notesTitleColumnName} LIKE ? and ${Constants.notesIsCompletedColumnName} = ${0}',
         whereArgs: ['%$title%']);
+    return data.map((row) => Note.fromMap(row)).toList();
+  }
+
+  Future<bool> markAsCompleted(Note object) async {
+    int countItemsUpdated = await database!.update(
+        Constants.notesTableName, {Constants.notesIsCompletedColumnName: 1},
+        where: '${Constants.notesIdColumnName} = ?', whereArgs: [object.id]);
+    return countItemsUpdated > 0;
+  }
+
+  Future<List<Note>> readCompleted() async {
+    List<Map<String, Object?>> data = await database!.query(
+        Constants.notesTableName,
+        where: '${Constants.notesIsCompletedColumnName} = ?',
+        whereArgs: [1]);
     return data.map((row) => Note.fromMap(row)).toList();
   }
 }
