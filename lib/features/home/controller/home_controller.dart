9856ce576f3bd788d7e0f5bae4040de009ccf490
mstfa_201 <mostafaa.sh2001@gmail.com>
2025-03-28 13:52:34 +0200

    remove comments

--- lib/features/home/controller/home_controller.dart
+++ lib/features/home/controller/home_controller.dart
@@ -4,21 +4,91 @@ import 'package:intl/intl.dart';
 import '../../../../core/storage/local/database/controller/note_database_controller.dart';
 import '../../../../core/widgets/custom_snackbar.dart';
 import '../../../../core/storage/local/database/model/note.dart';
-import '../../../config/constants.dart';
 
 class HomeController extends GetxController with CustomSnackBar {
   DateTime currentDate = DateTime.now();
   DateTime selectedDate = DateTime.now();
   List<Note> notes = [];
   List<Note> completedNotes = [];
-
   final NoteDatabaseController _noteDatabaseController =
       NoteDatabaseController();
+  TextEditingController? titleController;
+  TextEditingController? descController;
+  TextEditingController? dateController;
+  TextEditingController? timeController;
 
-  // Future<void> show(int noteId) async {
-  //   currentNote = await _noteDatabaseController.show(noteId) ?? Note();
-  //   update();
-  // }
+  String? selectedOption;
+  List<String> repeatOptions = [
+    "Once",
+    "Daily",
+    "Weekly",
+    "Monthly",
+  ];
+
+  @override
+  void onInit() {
+    super.onInit();
+    titleController = TextEditingController();
+    descController = TextEditingController();
+    dateController = TextEditingController();
+    timeController = TextEditingController();
+    read();
+  }
+
+  void initControllers(noteIndex) {
+    Note note = notes[noteIndex];
+    titleController!.text = note.title ?? '';
+    descController!.text = note.desc ?? '';
+    dateController!.text = note.date ?? '';
+    timeController!.text = note.time ?? '';
+    selectedOption = note.repeat;
+  }
+
+  void clearControllers() {
+    titleController!.clear();
+    descController!.clear();
+    dateController!.clear();
+    timeController!.clear();
+    selectedOption = null;
+  }
+
+  String getFullDate() => DateFormat('MMMM dd, y').format(currentDate);
+
+  String getDate(date) => DateFormat('dd').format(date);
+
+  String getDay(date) => DateFormat('EEE').format(date);
+
+  String getMonth(date) => DateFormat('MMM').format(date);
+
+  String getYear(date) => DateFormat('y').format(date);
+
+  void changeDay(DateTime date) {
+    selectedDate = date;
+    update();
+  }
+
+  Future<void> selectDate(context) async {
+    DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now().subtract(const Duration(days: 365)),
+        lastDate: DateTime.now().add(const Duration(days: 365)));
+    if (selectedDate != null) {
+      dateController?.text = DateFormat('MMMM dd, y').format(selectedDate);
+      update();
+    }
+  }
+
+  Future<void> selectTime(context) async {
+    TimeOfDay? selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (selectedTime != null) {
+      timeController!.text = selectedTime.format(context).toString();
+      update();
+    }
+  }
 
   Future<void> read() async {
     notes.clear();
@@ -60,73 +130,6 @@ class HomeController extends GetxController with CustomSnackBar {
     return false;
   }
 
-  String getFullDate() => DateFormat('MMMM dd, y').format(currentDate);
-
-  String getDate(date) => DateFormat('dd').format(date);
-
-  String getDay(date) => DateFormat('EEE').format(date);
-
-  String getMonth(date) => DateFormat('MMM').format(date);
-
-  String getYear(date) => DateFormat('y').format(date);
-
-  void changeDay(DateTime date) {
-    selectedDate = date;
-    update();
-  }
-
-  @override
-  void onInit() {
-    super.onInit();
-    titleController = TextEditingController();
-    descController = TextEditingController();
-    dateController = TextEditingController();
-    timeController = TextEditingController();
-    read();
-  }
-
-  TextEditingController? titleController;
-  TextEditingController? descController;
-  TextEditingController? dateController;
-  TextEditingController? timeController;
-  // final HomeController _homeController = Get.find<HomeController>();
-
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
-
-  String? selectedOption;
-  List<String> repeatOptions = [
-    "Once",
-    "Daily",
-    "Weekly",
-    "Monthly",
-  ];
-
-  Future<void> selectDate(context) async {
-    DateTime? selectedDate = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime.now().subtract(const Duration(days: 365)),
-        lastDate: DateTime.now().add(const Duration(days: 365)));
-    if (selectedDate != null) {
-      dateController?.text = DateFormat('yyyy-MM-dd').format(selectedDate);
-      update();
-    }
-  }
-
-  Future<void> selectTime(context) async {
-    TimeOfDay? selectedTime = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    );
-    if (selectedTime != null) {
-      timeController!.text = selectedTime.format(context).toString();
-      update();
-    }
-  }
-
   Future<void> performCreation() async {
     Note note = Note(
         title: titleController!.text.toString(),
@@ -138,32 +141,10 @@ class HomeController extends GetxController with CustomSnackBar {
     if (isCreated) {
       print('New Note Created Successfully');
       Get.back();
-      print("Title: ${titleController!.text}");
-      print("Desc: ${descController!.text}");
-      print("Date: ${dateController!.text}");
-      print("Time: ${timeController!.text}");
-
       update();
     } else {
       print('Creating Failed!!');
     }
-  }
-
-  void initControllers(noteIndex) {
-    Note note = notes[noteIndex];
-    titleController!.text = note.title ?? '';
-    descController!.text = note.desc ?? '';
-    dateController!.text = note.date ?? '';
-    timeController!.text = note.time ?? '';
-    selectedOption = note.repeat;
-  }
-
-  void clearControllers() {
-    titleController!.clear();
-    descController!.clear();
-    dateController!.clear();
-    timeController!.clear();
-    selectedOption = null;
   }
 
   Future<void> performUpdate(int index) async {
