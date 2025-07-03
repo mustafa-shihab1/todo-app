ce74059afb602450ddd545f0570fd10316b0d024
mstfa_201 <mostafaa.sh2001@gmail.com>
2025-07-03 18:11:54 +0300

    config scheduled notifications

--- lib/core/notification/notification_service.dart
+++ lib/core/notification/notification_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 class NotificationService {
   final notificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -9,34 +11,43 @@ class NotificationService {
   Future<void> initNotification() async {
     if (_isInitialized) return; // prevent re-initialization
 
-// prepare android init settings
-const initSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    // init timezone
+    tz.initializeTimeZones();
+    final String currentTimeZone = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(currentTimeZone));
 
-// prepare ios init settings
-    const initSettingsIOS = DarwinInitializationSettings( requestAlertPermission: true, requestBadgePermission: true, requestSoundPermission: true,
+    // prepare android init settings
+    const initSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    // prepare ios init settings
+    const initSettingsIOS = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
     );
 
-// init settings
+    // init settings
     const initSettings = InitializationSettings(
-    android: initSettingsAndroid,
-    iOS: initSettingsIOS,
+      android: initSettingsAndroid,
+      iOS: initSettingsIOS,
     );
 
-// initialize the plugin
+    // initialize the plugin
     await notificationsPlugin.initialize(initSettings);
   }
 
 // NOTIFICATIONS DETAIL SETUP
-NotificationDetails notificationDetails() {
+  NotificationDetails notificationDetails() {
     return const NotificationDetails(
-        android: AndroidNotificationDetails(
+      android: AndroidNotificationDetails(
         'daily_channel_id',
         'Daily Notifications',
         channelDescription: 'Daily Notification Channel',
         importance: Importance.max,
         priority: Priority.high,
-        ),
-    iOS: DarwinNotificationDetails(),
+      ),
+      iOS: DarwinNotificationDetails(),
     );
   }
 
@@ -56,6 +67,36 @@ NotificationDetails notificationDetails() {
     );
   }
 
+// SHOW SCHEDULED NOTIFICATION
+  Future<void> scheduledNotification({
+    required int id,
+    required String title,
+    required String body,
+    required String hour,
+    required String minute,
+  }) async {
+    // Get the current timezone
+    final now = tz.TZDateTime.now(tz.local);
 
+    // create timezone for today at specified hour and minute
+    var scheduledDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      int.parse(hour),
+      int.parse(minute),
+    );
 
+    // Schedule the notification
+    await notificationsPlugin.zonedSchedule(
+        id, title, body, scheduledDate, const NotificationDetails(),
+        matchDateTimeComponents: DateTimeComponents.time,
+        androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle);
+  }
+
+  // Cancle all notifications
+  Future<void> cancelAllNotifications() async {
+    await notificationsPlugin.cancelAll();
+  }
 }
